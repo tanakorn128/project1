bc59c1de1437778a7465bb4ca57088ec86467654
tanakorn128 <60030041@kmitl.ac.th>
2018-11-24 16:33:26 +0700

    update login

--- g-callback.php
+++ g-callback.php
@@ -3,7 +3,7 @@
     require_once "GoogleLoginx/config.php";
     require 'connect.php';
 
-    header('Location: Homeuser1.php');
+   // header('Location: Homeuser1.php');
      print($_GET['code']);
     $gClient->authenticate($_GET['code']);
     
@@ -14,17 +14,47 @@
  
      $name=$token_data['name'];
     $email=$token_data['email'];
+    
       
 ?>
 <?php
-       $q ="INSERT INTO  googlelogin(gmail,g_name) VALUES ('$email','$name')";
-       $result = mysqli_query($Con,$q);
+      // $q ="INSERT INTO  googlelogin(gmail,g_name) VALUES ('$email','$name')";
+      // $result = mysqli_query($Con,$q);
        
     ?>
-    
-    <?php
 
-    ?>
+    <?php
+ require 'connect.php';
+
+ $q ="SELECT * FROM user ";
+
+ $result = mysqli_query($Con,$q);
+ 
+ ?>
+ <?php
+while ($row = mysqli_fetch_array($result)){
+   $sum = $row['gmail'];
+   $lv = $row['Levels'];
+   if($email == $sum){
+    if($lv== "admin"){
+        header('Location: Homeuser0.php');
+    }
+    if($lv == "member"){
+        header('Location: Homeuser1.php');
+    }
+ }
+ else{
+    header('Location: Homeuser2.php');
+ }
+ }
+ ?>
+ 
+ 
+
+ 
+
+
+
        
      
     
